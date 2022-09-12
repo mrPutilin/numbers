63eb34dd1ed81be6401787e4784a54ba93e3b6c2
Evgeniy Putilin <mr.putilinz@gmail.com>
2022-09-12 17:04:35 +0300

    3rd commit

--- StreamMain.java
+++ StreamMain.java
@@ -12,7 +12,8 @@ public class StreamMain {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> stream = intList.stream()
                 .filter(x -> x > 0)
-                .filter(x -> x % 2 == 0).toList();
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder()).toList();
         System.out.println(stream);
 
 
