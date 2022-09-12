af352d6bfab4894d7d9d06509ba5fc46218fee8c
Evgeniy Putilin <mr.putilinz@gmail.com>
2022-09-12 18:06:20 +0300

    Final commit

--- StreamMain.java
+++ StreamMain.java
@@ -3,16 +3,17 @@ package core.hw2.numbers;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 public class StreamMain {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> stream = intList.stream()
+        intList.stream()
                 .filter(x -> x > 0)
                 .filter(x -> x % 2 == 0)
-                .sorted(Comparator.naturalOrder()).toList();
-        System.out.println(stream);
+                .sorted(Comparator.naturalOrder())
+                .forEach(x -> System.out.print(x + " "));
 
 
     }
