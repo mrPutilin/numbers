2f0bafbc95760201895daed428803ea090894f94
Evgeniy Putilin <mr.putilinz@gmail.com>
2022-09-12 15:02:23 +0300

    Initial coomit

---
+++ Numbers.java
@@ -0,0 +1,22 @@
+package core.hw2.numbers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Numbers {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        List<Integer> result = new ArrayList();
+        for (int x : input) {
+            if (x > 0 && x % 2 == 0) {
+                result.add(x);
+
+            }
+
+        }
+        Collections.sort(result);
+        System.out.println(result);
+
+    }
+}
